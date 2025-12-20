ca44d8b94673d1b3b69435260fdaa3204074cb23
Reiji <reiji@reiji.localdomain>
2025-12-20 12:40:05 +0900

    Devise関連のメッセージを日本語化

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,8 @@ module TechlogApp
       g.assets false          # CSS, JavaScriptファイルを自動生成しない
       g.helper false      # helperファイルを自動生成しない
       g.test_framework :rspec # テストフレームワークをrspecに設定
-    end  
+    end
+
+    config.i18n.default_locale = :ja
   end
 end
